425f89412015512b97227eff6b56cc1b3e9cacc7
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-01-31 10:38:28 +0000

    admin login

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -2,7 +2,9 @@
     <div class="footer">
     <div class="container footer-container">
         &copy; <?=$wedding_name.' ' .date('Y');?>
+        <a href="/admin" target="+blank"><i class="fa-solid fa-laptop-code"></i></a>
         <p>Website developed with love by <a href="https://www.parrotmedia.co.uk" target="_blank">Parrot Media</a></p>
+        
     </div>
     </div>
 </footer>
