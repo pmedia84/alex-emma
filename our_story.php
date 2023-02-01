1b8d5e268751ab4bed127a9eb0da1d2aaadd577b
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-01 09:53:58 +0000

    story

--- our_story.php
+++ our_story.php
@@ -1,4 +1,8 @@
-<?php include("inc/header.inc.php"); ?>
+<?php include("inc/header.inc.php");
+$story_query = ('SELECT * FROM wedding_story LIMIT  1');
+$story = $db->query($story_query);
+$story_result = $story->fetch_assoc();
+?>
 <!-- All above this is for each page -->
 <title>The Wedding of <?= $wedding_name; ?> | Our Story</title>
 </head>
@@ -10,28 +14,25 @@
             <div class="hero-title text-center ">
                 <h1>Our Story</h1>
                 <p><?= $wedding_name; ?></p>
-                
+
             </div>
             <div class="hero-img-card">
                 <img src="assets/img/hero/index-hero-card.jpg" alt="">
             </div>
         </div>
     </div>
-
     <main>
         <section class="container my-7 ">
-            <div class="std-card">
-                <h2 class="text-center">How it all began!</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quos dolorum consequatur dicta nemo porro omnis, vero autem voluptates laborum praesentium tempora exercitationem aliquam. Nesciunt est esse eius, atque quidem maiores possimus. At quos assumenda modi commodi nam odio earum fuga, voluptatem autem labore est, rerum reiciendis amet eius optio?</p>
-                <br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae aliquam esse assumenda doloremque ipsa facere voluptatum laboriosam et sint rerum recusandae id laborum obcaecati, dignissimos itaque, illo quae culpa dolorum tenetur. Adipisci, nihil. Qui repellendus tenetur, magnam rerum sed dolor adipisci corporis cupiditate et veritatis similique mollitia repudiandae facilis.</p>
+            <div class="std-card story">
+                <?php if ($story->num_rows > 0 && $story_result['story_status'] == "Published") :
+                    $story_body = html_entity_decode($story_result['story_body']); ?>
+                    <?= $story_body; ?>
+                <?php endif; ?>
             </div>
-
         </section>
-
-        
-<div class="my-7 section-divider">
-    <img src="assets/img/flowers.svg" alt="">
-</div>
+        <div class="my-7 section-divider">
+            <img src="assets/img/flowers.svg" alt="">
+        </div>
 
 
 
