3ffe573affd2c148a91b4c646b2b386e7fea7afb
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-01 14:03:26 +0000

    updates

--- inc/settings.php
+++ inc/settings.php
@@ -7,7 +7,7 @@
 $contactnum ="";
 $emailaddress = "";
 $facebookurl = "";
-
+include("../email_settings.php");
 //load database for wedding information
     $wedding_query = ('SELECT wedding_name, wedding_date FROM wedding LIMIT 1');
     $wedding_result = $db->query($wedding_query);
